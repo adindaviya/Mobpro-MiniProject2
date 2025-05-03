2a87ba739d775a4c683ffd13f7a29b7ca6a5e9ee
Adinda Alif Viya <adindaalifviya@gmail.com>
2025-05-04 00:29:50 +0700

    Membuat tampilan list

--- app/src/main/java/com/adindaviya0052/miniproject2/ui/screen/MainScreen.kt
+++ app/src/main/java/com/adindaviya0052/miniproject2/ui/screen/MainScreen.kt
@@ -1,8 +1,15 @@
 package com.adindaviya0052.miniproject2.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,8 +18,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.adindaviya0052.miniproject2.R
+import com.adindaviya0052.miniproject2.model.Film
 import com.adindaviya0052.miniproject2.ui.theme.MiniProject2Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,10 +50,50 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(data) {
+            ListItem(film = it)
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(film: Film) {
+    Column (
+        modifier = Modifier.fillMaxWidth().padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = film.judul,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp
+        )
+        Text(
+            text = film.review,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = film.jenis,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = film.status,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = film.tanggal)
+    }
 }
 
 @Preview(showBackground = true)
